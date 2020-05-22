c033d0c4c9566d02ecad1e2ea72926cd7d3bf6cc
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-22 10:37:43 +0300

    created ZenQuote

---
+++ src/DadJokes/ZenQuote.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ZenQuote extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ZenQuote;
