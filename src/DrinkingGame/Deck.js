4e5d4716601a9df9d34e256e946be0ffa6455a59
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-24 16:35:04 +0300

    got the data with AJAX

--- src/DrinkingGame/Deck.js
+++ src/DrinkingGame/Deck.js
@@ -1,38 +1,74 @@
-import React, { Component } from 'react';
-import WorkIsInProgress from '../images/workIsInProgress.png';
-import InnerNavBar from '../InnerNavBar/InnerNavBar';
+import React, { Component } from "react";
+import WorkIsInProgress from "../images/workIsInProgress.png";
+import InnerNavBar from "../InnerNavBar/InnerNavBar";
 import axios from "axios";
 
-const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle"
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            deck: null,
-            drawnCards: [] 
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      deck: null,
+      drawnCards: [],
+    };
+    this.getCard = this.getCard.bind(this);
+  }
 
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
+    // console.log(deck)
+    // console.log(deck.data)
+
+    this.setState({ deck: deck.data });
+  }
+
+  async getCard() {
+    // make request using deck id
+    let id = this.state.deck.deck_id;
+    // console.log(id)
+
+    // we gotta use try catch because when running out of deck it gives error
+    try {
+      let cardUrl = `${API_BASE_URL}/${id}/draw`;
+      let cardRes = await axios.get(cardUrl);
+      // console.log(cardRes.data)
+      if (!cardRes.data.success) {
+        throw new Error("No Cards Left!");
+      }
+
+      // set state using new card info from api
+      let card = cardRes.data.cards[0];
+      this.setState((oldState) => ({
+        drawnCards: [
+          ...oldState.drawnCards,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.suit} of ${card.value}`,
+          },
+        ],
+      }));
+    } catch (err) {
+      alert(err);
     }
+  }
 
-    async componentDidMount() {
-        let deck = await axios.get(API_URL);
-        // console.log(deck)
-        
-        this.setState({deck: deck.data})
-    }
-
-    render() {
-        return (
-            <div>
-                <InnerNavBar
+  render() {
+    return (
+      <div>
+        <InnerNavBar
           text="back"
           link="https://github.com/DundarKoray/React__Project__MyAppCollections/tree/master/src/DadJokes"
-          />
-                <img src={WorkIsInProgress} />
-            </div>
-        );
-    }
+        />
+        <img src={WorkIsInProgress} />
+        <h1>Drinking Game</h1>
+        <button onClick={this.getCard} className="btn-primary">
+          Get Card
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Deck;
