2a067fc809211c36e7232423d85ae1603ad8b8e7
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-25 22:28:05 +0300

    got new jokes from the API

--- src/DadJokes/JokeList.js
+++ src/DadJokes/JokeList.js
@@ -3,14 +3,37 @@ import InnerNavBar from "../InnerNavBar/InnerNavBar";
 import WorkIsInProgress from "../images/workIsInProgress.png";
 import Joke from "./Joke";
 import axios from "axios";
+import styles from "./joke.module.css";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [],
+    };
+  }
+
   async componentDidMount() {
-    // Load Jokes
-    let response = await axios.get("https://icanhazdadjoke.com/", {
-      headers: { Accept: "application/json" },
+    // Load Jokes and store 10 of them
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+
+      // console.log(response.data.joke);
+      jokes.push(response.data.joke);
+    }
+    // console.log(jokes)
+    this.setState({
+      jokes: jokes,
     });
-    console.log(response.data.joke);
+
+    console.log(this.state.jokes);
   }
   render() {
     return (
@@ -20,7 +43,13 @@ class JokeList extends Component {
           link="https://github.com/DundarKoray/React__Project__MyAppCollections/tree/master/src/DadJokes"
         />
         <img style={{ width: "250px" }} src={WorkIsInProgress} />
-        <Joke />
+        <div className={styles.jokeWrap}>
+          <h1>Dad Jokes</h1>
+          <div className={styles.joke}></div>
+          {this.state.jokes.map((j) => {
+            return <div>{j}</div>;
+          })}
+        </div>
       </div>
     );
   }
