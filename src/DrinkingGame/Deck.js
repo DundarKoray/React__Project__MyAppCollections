b9f17ee088e8f8c9639b3b67ea70863e05866651
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-25 13:25:21 +0300

    applied media querries for drinking game

--- src/DrinkingGame/Deck.js
+++ src/DrinkingGame/Deck.js
@@ -61,43 +61,80 @@ class Deck extends Component {
       return <Card name={item.name} image={item.image} key={item.id} />;
     });
     return (
-        <div>
-
+      <div>
         <InnerNavBar
           text="back"
           link="https://github.com/DundarKoray/React__Project__MyAppCollections/tree/master/src/DrinkingGame"
-          />
-                  <div className={styles.deckWrapper}>
-        {/* <img src={WorkIsInProgress} /> */}
-        <h1>Drinking Game</h1>
-        <p>Play Responsibly!</p>
-        <div className={styles.cardWrapper}>{cards}</div>
-        <div style={{margin:"auto",width:"250px",height: "300px", border:"1px solid green"}}></div>
-        <button onClick={this.getCard} className={styles.deckButton}>
-          Get Card
-        </button>
-        <div className={styles.rules}>
-
-        <h2>Rules</h2>
-        <ul style={{padding:"1rem",width:"90%", textAlign:"left", margin: "auto", listStyle:"none"}}>
-            <li>A: Waterfall - Every player begins drinking, and no one can stop until the player before them does </li>
-            <li>2: The person who picks the card picks someone else to drink. </li>
-            <li>3: The person who picks the card drinks. </li>
-            <li>4: Everyone races to touch their nose, last person to do so drinks</li>
-            <li>5: Everyone races to tell their names, last person to do so drinks</li>
-            <li>6: Everybody drinks</li>
-            <li>7: Pick a person to drink with</li>
-            <li>8: Rhyme - Say a phrase, and everyone else must say phrases that rhyme</li>
-            <li>9: Whoever drew the card can pick two person to drink </li>
-            <li>10:  Categories - Pick a category, and say something from that category (i.e. if "drinking games" was the category, "kings" would be a viable answer. </li>
-            <li>J: The person to your right has to drink. </li>
-            <li>Q: There should be an empty glass, everyone who picks queen should pour their drink in the empty glass, fill the quarter. 4 mix drinks</li>
-            <li>K: Ruler - Make a rule that everyone must follow until the next King is drawn (i.e. force everyone to drink after each turn) </li>
-            
-        </ul>
-      </div>
-        </div>
+        />
+        <div className={styles.deckWrapper}>
+          {/* <img src={WorkIsInProgress} /> */}
+          <h1>🍻 Drinking Game 🍻</h1>
+          <h2>◆ Play Responsibly!◆ </h2>
+          <div className={styles.cardWrapper}>{cards}</div>
+          <div
+            style={{
+              margin: "auto",
+              width: "250px",
+              height: "300px",
+              border: "1px solid green",
+            }}
+          ></div>
+          <button onClick={this.getCard} className={styles.deckButton}>
+            Get Card
+          </button>
+          <div className={styles.rules}>
+            <h2>Rules</h2>
+            <ul
+              style={{
+                padding: "1rem",
+                width: "90%",
+                textAlign: "left",
+                margin: "auto",
+                listStyle: "none",
+              }}
+            >
+              <li>
+                A: Waterfall - Every player begins drinking, and no one can stop
+                until the player before them does{" "}
+              </li>
+              <li>
+                2: The person who picks the card picks someone else to drink.{" "}
+              </li>
+              <li>3: The person who picks the card drinks. </li>
+              <li>
+                4: Everyone races to touch their nose, last person to do so
+                drinks
+              </li>
+              <li>
+                5: Everyone races to tell their names, last person to do so
+                drinks
+              </li>
+              <li>6: Everybody drinks</li>
+              <li>7: Pick a person to drink with</li>
+              <li>
+                8: Rhyme - Say a phrase, and everyone else must say phrases that
+                rhyme
+              </li>
+              <li>9: Whoever drew the card can pick two person to drink </li>
+              <li>
+                10: Categories - Pick a category, and say something from that
+                category (i.e. if "drinking games" was the category, "kings"
+                would be a viable answer.{" "}
+              </li>
+              <li>J: The person to your right has to drink. </li>
+              <li>
+                Q: There should be an empty glass, everyone who picks queen
+                should pour their drink in the empty glass, fill the quarter. 4
+                mix drinks
+              </li>
+              <li>
+                K: Ruler - Make a rule that everyone must follow until the next
+                King is drawn (i.e. force everyone to drink after each turn){" "}
+              </li>
+            </ul>
           </div>
+        </div>
+      </div>
     );
   }
 }
