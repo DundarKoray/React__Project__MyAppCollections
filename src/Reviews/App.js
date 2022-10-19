7141e602e8fb5bfe1cfef671d405e4fed2e38397
Koray Dundar <koraydundar_40@hotmail.com>
2022-10-19 20:33:35 +0300

    created structure for the review app

--- src/Reviews/App.js
+++ src/Reviews/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import SimpleHero from "../SimpleHero/SimpleHero";
+import Banner from "../Banner/Banner";
+import { useState, useEffect } from 'react'
+import InnerNavBar from "../InnerNavBar/InnerNavBar";
+import styles from './app.module.scss';
 import Review from './Review';
 function App() {
   return <h2>reviews project setup</h2>;
