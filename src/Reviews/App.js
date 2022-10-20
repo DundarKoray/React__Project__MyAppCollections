533f267867b42f67977148fb8689f2cbf449acf1
Koray Dündar Personal <koraydundar_40@hotmail.com>
2022-10-20 20:24:27 +0300

    added more goal description

--- src/Reviews/App.js
+++ src/Reviews/App.js
@@ -4,22 +4,24 @@ import { useState, useEffect } from 'react'
 import InnerNavBar from "../InnerNavBar/InnerNavBar";
 import styles from './app.module.scss';
 import Review from './Review';
+
 function App() {
-  return (
-    <div>
-        <SimpleHero>
-            <Banner>
-                <InnerNavBar />
-                <div className={styles.goals}>
-                    Goals
-                    <br />1: Fetch the data from JSON API and list only one review at a time.
-                    <br />2: Add a button for showing previous and next reviews.
-                    <br />2: Add a button which shows a random button.
-                </div>
-            </Banner>    
-        </SimpleHero>            
-    </div>
-);
+
+    return (
+        <div>
+            <SimpleHero>
+                <Banner>
+                    <InnerNavBar />
+                    <div className={styles.goals}>
+                        Goals
+                        <br />1: Fetch the data from local JSON API and list only one review at a time.
+                        <br />2: Add buttons by using React Icons for showing previous and next review.
+                        <br />2: Add a button which shows a random button.
+                    </div>
+                </Banner>    
+            </SimpleHero>            
+        </div>
+    );
 }
 
 export default App;
