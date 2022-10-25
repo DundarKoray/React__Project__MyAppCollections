9d68ced21679b719744315af642ba22de2cef52b
Koray Dundar <koraydundar_40@hotmail.com>
2022-10-25 20:39:46 +0300

    styled and completed the logic¨

--- src/FrequentlyAskedQuestions/Question.js
+++ src/FrequentlyAskedQuestions/Question.js
@@ -4,11 +4,20 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import styles from './question.module.scss';
 
 const Question = ({id, title, info}) => {
+  const [showInfo, setShowInfo] = useState(false)
   
   return (
-    <div className={styles.questionContainer}>
-      <h4 className={styles.question}>{title}</h4>
-      <AiOutlinePlus />
+    <div onClick={()=> {setShowInfo(!showInfo)}} className={styles.questionAndAnswer}>
+      <div className={styles.questionContainer}>
+        <h4 className={styles.question}>{title}</h4>
+        <button onClick={()=> {setShowInfo(!showInfo)}}>{!showInfo ? <AiOutlinePlus /> :  <AiOutlineMinus />}</button>
+      </div>
+      {showInfo ?
+        <div className={styles.answer}>
+          <p>i{info}</p>
+        </div>
+        : null
+      }
     </div>
   );
 };
